a60afa0f93b7fb4e9b2da1e3cbfe1405f48e9798
jornh <jornhansen@gmail.com>
2019-11-02 22:59:11 +0100

    Enable redux devtools browser extension

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,11 @@ import reducer from '../reducers';
 import { saveState } from '../utils';
 
 export default function configureStore(initialState) {
-  const store = createStore(reducer, initialState);
+  const store = createStore(
+    reducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
   store.subscribe(() => {
     const { parameters, textLabels, theme, view } = store.getState();
